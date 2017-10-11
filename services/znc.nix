b60586bf6765c88bb92e411977ab421ca84c322d
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-10-12 00:51:03 +0800

    Add #idris

--- services/znc.nix
+++ services/znc.nix
@@ -7,7 +7,7 @@
     confOptions = {
       networks = {
         freenode = {
-          channels = [ "haskell" "haskell-beginners" "haskell-offtopic" "nixos" "purescript" "qfpl" ];
+          channels = [ "haskell" "haskell-beginners" "haskell-offtopic" "idris" "nixos" "purescript" "qfpl" ];
           modules = [ "log" "simple_away" ];
           server = "chat.freenode.net";
           port = 6697;
