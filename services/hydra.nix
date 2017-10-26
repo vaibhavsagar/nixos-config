e44aa1f5b3b93074d1ee9b2350b456eb05a7873c
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-10-26 08:35:32 +0000

    services/hydra: configure localhost builder and binary cache

--- services/hydra.nix
+++ services/hydra.nix
@@ -1,9 +1,19 @@
 { config, pkgs, ... }: {
   networking.firewall.allowedTCPPorts = [ 3000 ];
+  nix.buildMachines = [
+    {
+      hostName = "localhost";
+      systems = [ "i686-linux" "x86_64-linux" ];
+      maxJobs = 1;
+    }
+  ];
   services.hydra = {
     enable = true;
     hydraURL = "http://128.199.234.106";
     notificationSender = "hydra@vaibhavsagar.com";
     useSubstitutes = true;
+    extraConfig = ''
+      store_uri = file:///var/lib/hydra/cache?secret-key=/etc/nix/128.199.234.106/nix-serve.sec
+    '';
   };
 }
