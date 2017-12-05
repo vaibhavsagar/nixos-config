82f309ba4dfc9b09ca483bb021ae1fd2b9b6b978
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-12-05 20:35:29 +0800

    Enable virtualbox

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -120,4 +120,9 @@
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "17.09";
 
+  virtualisation.virtualbox = {
+    guest.enable = true;
+    host.enable = true;
+  };
+
 }
