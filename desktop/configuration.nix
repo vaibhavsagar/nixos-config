43761d431e1eb29ed80ef90d6669152fec56ad56
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-04-22 18:30:20 +0800

    desktop: add emoji

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -68,6 +68,10 @@
     wget
   ];
 
+  fonts.fonts = with pkgs; [
+    noto-fonts-emoji
+  ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.bash.enableCompletion = true;
