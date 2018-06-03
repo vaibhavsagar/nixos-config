c933a29f67626c01a990c9d09210bf0063cea382
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-06-03 16:09:44 +0800

    laptop: add ghcid to environment.systemPackages

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -53,6 +53,10 @@
     cabal-install
     cabal2nix
     firefox-beta-bin
+    (haskellPackages.extend (self: super: {
+      ghcid = self.callHackage "ghcid" "0.7" {};
+      extra = self.callHackage "extra" "1.6.6" {};
+    })).ghcid
     gimp
     git
     git-crypt
