4a747fd8b37b249004b02523ceb05416d9207d37
Vaibhav Sagar <vaibhavsagar@gmail.com>
2025-08-18 12:30:16 +1000

    l14g3: add build machine

--- l14g3/configuration.nix
+++ l14g3/configuration.nix
@@ -144,6 +144,19 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  nix.distributedBuilds = true;
+  nix.settings.builders-use-substitutes = true;
+
+  nix.buildMachines = [
+    {
+      hostName = "100.124.243.32";
+      sshUser = "vaibhavsagar";
+      sshKey = "/home/vaibhavsagar/.ssh/id_ed25519";
+      system = pkgs.stdenv.hostPlatform.system;
+      supportedFeatures = [ "nixos-test" "big-parallel" "kvm" ];
+    }
+  ];
+
   nix.settings = {
     auto-optimise-store = true;
     substituters = [
